e4beae6b1e66e5815ccdf1ecba677f9817ff8902
Martin Gilles <martin.gilles@gundlach-automotive.com>
2022-02-28 12:33:09 +0100

    finished day_19 part 1

---
+++ adventofcode_2018/day_19/day_19.go
@@ -0,0 +1,73 @@
+package day_19
+
+import (
+	"adventofcode_2018/errutil"
+	"adventofcode_2018/readutil"
+	"fmt"
+	"strings"
+
+	"github.com/mazzegi/slices"
+	"github.com/pkg/errors"
+)
+
+func log(pattern string, args ...interface{}) {
+	fmt.Printf(pattern+"\n", args...)
+}
+
+func fatal(pattern string, args ...interface{}) {
+	panic(fmt.Sprintf(pattern+"\n", args...))
+}
+
+func Part1() {
+	res, err := part1MainFunc(input, inputIP)
+	errutil.ExitOnErr(err)
+	log("part1: result = %d", res)
+}
+
+func Part2() {
+	res, err := part2MainFunc(input, inputIP)
+	errutil.ExitOnErr(err)
+	log("part2: result = %d", res)
+}
+
+//
+
+func parseInstructions(in string) ([]Instr, error) {
+	var iss []Instr
+	for _, line := range readutil.ReadLines(in) {
+		fs := strings.Fields(line)
+		if len(fs) != 4 {
+			return nil, errors.Errorf("cannot parse %q", line)
+		}
+		args, err := slices.Convert(fs[1:], slices.ParseInt)
+		if err != nil {
+			return nil, errors.Wrapf(err, "convert args %q", line)
+		}
+		iss = append(iss, Instr{fs[0], args})
+	}
+	return iss, nil
+}
+
+func part1MainFunc(in string, ip int) (int, error) {
+	iss, err := parseInstructions(in)
+	if err != nil {
+		return 0, err
+	}
+
+	m := NewMachine(ip, [6]int{0, 0, 0, 0, 0, 0}, iss)
+	m.Run()
+
+	return m.regs[0], nil
+}
+
+func part2MainFunc(in string, ip int) (int, error) {
+	iss, err := parseInstructions(in)
+	if err != nil {
+		return 0, err
+	}
+
+	m := NewMachine(ip, [6]int{1, 0, 0, 0, 0, 0}, iss)
+	m.Run()
+
+	return m.regs[0], nil
+}
---
+++ adventofcode_2018/day_19/day_19_test.go
@@ -0,0 +1,36 @@
+package day_19
+
+import (
+	"adventofcode_2018/testutil"
+	"testing"
+)
+
+const inputTestIP = 0
+
+const inputTest = `
+seti 5 0 1
+seti 6 0 2
+addi 0 1 0
+addr 1 2 3
+setr 1 0 0
+seti 8 0 4
+seti 9 0 5
+`
+
+func TestPart1MainFunc(t *testing.T) {
+	res, err := part1MainFunc(inputTest, inputTestIP)
+	testutil.CheckUnexpectedError(t, err)
+	var exp int = 6
+	if exp != res {
+		t.Fatalf("want %d, have %d", exp, res)
+	}
+}
+
+func TestPart2MainFunc(t *testing.T) {
+	res, err := part2MainFunc(inputTest, inputTestIP)
+	testutil.CheckUnexpectedError(t, err)
+	var exp int = -42
+	if exp != res {
+		t.Fatalf("want %d, have %d", exp, res)
+	}
+}
