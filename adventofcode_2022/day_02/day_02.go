bb0b5a766b242ca4c6e6d046918ee160f3d3cdee
Martin Gilles <martin.gilles@gundlach-automotive.com>
2022-12-02 18:55:16 +0100

    2022 day_02

--- adventofcode_2022/day_02/day_02.go
+++ adventofcode_2022/day_02/day_02.go
@@ -1,9 +1,11 @@
-
 package day_02
 
 import (
-	"fmt"	
-	"github.com/mazzegi/adventofcode/errutil"	
+	"fmt"
+	"strings"
+
+	"github.com/mazzegi/adventofcode/errutil"
+	"github.com/mazzegi/adventofcode/readutil"
 )
 
 func log(pattern string, args ...interface{}) {
@@ -26,10 +28,162 @@ func Part2() {
 	log("part2: result = %d", res)
 }
 
-func part1MainFunc(in string) (int, error){
-	return 0, nil
+type Shape string
+
+const (
+	Rock     Shape = "rock"
+	Paper    Shape = "paper"
+	Scissors Shape = "scissors"
+)
+
+func (sh Shape) Score() int {
+	switch sh {
+	case Rock:
+		return 1
+	case Paper:
+		return 2
+	case Scissors:
+		return 3
+	default:
+		fatal("invalid shape %q", sh)
+		return 0
+	}
 }
 
-func part2MainFunc(in string) (int, error){
-	return 0, nil
+func (sh Shape) Defeats(osh Shape) bool {
+	switch sh {
+	case Rock:
+		return osh == Scissors
+	case Paper:
+		return osh == Rock
+	case Scissors:
+		return osh == Paper
+	default:
+		fatal("invalid shape %q", sh)
+		return false
+	}
+}
+
+func (sh Shape) DefeatsAgainst() Shape {
+	switch sh {
+	case Rock:
+		return Scissors
+	case Paper:
+		return Rock
+	case Scissors:
+		return Paper
+	default:
+		fatal("invalid shape %q", sh)
+		return ""
+	}
+}
+
+func (sh Shape) LoosesAgainst() Shape {
+	switch sh {
+	case Rock:
+		return Paper
+	case Paper:
+		return Scissors
+	case Scissors:
+		return Rock
+	default:
+		fatal("invalid shape %q", sh)
+		return ""
+	}
+}
+
+func myRoundScore(opp Shape, me Shape) int {
+	if opp == me {
+		return 3
+	}
+	if opp.Defeats(me) {
+		return 0
+	}
+	return 6
+}
+
+type shapeMapping map[string]Shape
+
+func (sm shapeMapping) mustLookup(s string) Shape {
+	sh, ok := sm[s]
+	if !ok {
+		fatal("lookup %q", s)
+	}
+	return sh
+}
+
+type shapePair struct {
+	opp Shape
+	me  Shape
+}
+
+func myScore(opp Shape, me Shape) int {
+	return myRoundScore(opp, me) + me.Score()
+}
+
+func part1MainFunc(in string) (int, error) {
+	mappingOpp := shapeMapping{
+		"A": Rock,
+		"B": Paper,
+		"C": Scissors,
+	}
+	mappingMe := shapeMapping{
+		"X": Rock,
+		"Y": Paper,
+		"Z": Scissors,
+	}
+	var pairs []shapePair
+	lines := readutil.ReadLines(in)
+	for _, line := range lines {
+		sl := strings.Fields(line)
+		if len(sl) != 2 {
+			fatal("invalid line %q", line)
+		}
+		pairs = append(pairs, shapePair{
+			opp: mappingOpp.mustLookup(strings.TrimSpace(sl[0])),
+			me:  mappingMe.mustLookup(strings.TrimSpace(sl[1])),
+		})
+	}
+	var score int
+	for _, pair := range pairs {
+		score += myScore(pair.opp, pair.me)
+	}
+	return score, nil
+}
+
+func part2MainFunc(in string) (int, error) {
+	mappingOpp := shapeMapping{
+		"A": Rock,
+		"B": Paper,
+		"C": Scissors,
+	}
+	var pairs []shapePair
+	lines := readutil.ReadLines(in)
+	for _, line := range lines {
+		sl := strings.Fields(line)
+		if len(sl) != 2 {
+			fatal("invalid line %q", line)
+		}
+		opp := mappingOpp.mustLookup(strings.TrimSpace(sl[0]))
+		outcome := strings.TrimSpace(sl[1])
+		pair := shapePair{
+			opp: opp,
+		}
+		switch outcome {
+		case "X": // have to loose
+			pair.me = opp.DefeatsAgainst()
+		case "Y": // need draw
+			pair.me = opp
+		case "Z": //have to win
+			pair.me = opp.LoosesAgainst()
+		default:
+			fatal("unknown outcome %q", outcome)
+		}
+		pairs = append(pairs, pair)
+	}
+	var score int
+	for _, pair := range pairs {
+		score += myScore(pair.opp, pair.me)
+	}
+	return score, nil
 }
--- adventofcode_2022/day_02/day_02_test.go
+++ adventofcode_2022/day_02/day_02_test.go
@@ -1,17 +1,21 @@
-
 package day_02
 
-import (	
+import (
 	"testing"
+
 	"github.com/mazzegi/adventofcode/testutil"
 )
 
-const inputTest = ""
+const inputTest = `
+A Y
+B X
+C Z
+`
 
 func TestPart1MainFunc(t *testing.T) {
 	res, err := part1MainFunc(inputTest)
 	testutil.CheckUnexpectedError(t, err)
-	var exp int = -42
+	var exp int = 15
 	if exp != res {
 		t.Fatalf("want %d, have %d", exp, res)
 	}
@@ -20,7 +24,7 @@ func TestPart1MainFunc(t *testing.T) {
 func TestPart2MainFunc(t *testing.T) {
 	res, err := part2MainFunc(inputTest)
 	testutil.CheckUnexpectedError(t, err)
-	var exp int = -42
+	var exp int = 12
 	if exp != res {
 		t.Fatalf("want %d, have %d", exp, res)
 	}
