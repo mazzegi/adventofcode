8a2b8f95066d478315f05b378cf14d97304a4e1e
mazzegi <martin.gilles.42@gmail.com>
2025-12-23 17:41:48 +0100

    day 12 create

---
+++ adventofcode_2025/day_12/cmd/main.go
@@ -0,0 +1,9 @@
+
+package main
+
+import "github.com/mazzegi/adventofcode/adventofcode_2025/day_12"
+
+func main() {
+	day_12.Part1()
+	day_12.Part2()
+}
---
+++ adventofcode_2025/day_12/day_12_test.go
@@ -0,0 +1,96 @@
+package day_12
+
+import (
+	"testing"
+
+	"github.com/mazzegi/adventofcode/testutil"
+)
+
+const inputTest = `
+0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2
+`
+
+func TestPart1MainFunc(t *testing.T) {
+	res, err := part1MainFunc(inputTest)
+	testutil.CheckUnexpectedError(t, err)
+	var exp int = 2
+	if exp != res {
+		t.Fatalf("want %d, have %d", exp, res)
+	}
+}
+
+func TestPart2MainFunc(t *testing.T) {
+	res, err := part2MainFunc(inputTest)
+	testutil.CheckUnexpectedError(t, err)
+	var exp int = -42
+	if exp != res {
+		t.Fatalf("want %d, have %d", exp, res)
+	}
+}
+
+/*
+
+###
+##.
+##.
+
+ROT
+###
+##.
+##.
+
+###
+###
+..#
+
+.##
+.##
+###
+
+FLIP
+
+###
+.##
+.##
+
+..#
+###
+###
+
+##.
+##.
+###
+
+*/
